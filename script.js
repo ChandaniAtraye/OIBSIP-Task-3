8a199379fb6854e7ab4773592a5775f473b1cada
ChandaniAtraye <138958904+ChandaniAtraye@users.noreply.github.com>
2024-02-02 16:53:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function convertTemperature(){
+    const inputTemperature = parseFloat(document.getElementById("num").value);
+    const unit = document.getElementById("unit").value
+
+    let convertedTemperature;
+
+    if(unit === 'Celsius')
+    {
+        //convert tp fahreh
+
+        convertedTemperature = (inputTemperature * 9/5) + 32;
+        document.getElementById("result").textContent =`${inputTemperature} degrees Celsius = ${ConvertedTemperature} degrees Fahrenheit.`
+    }
+    else if(unit === 'Fahrenheit')
+     {
+        // convert to celsius
+        convertedTemperature = (inputTemperature - 32 ) * 5/9;
+        document.getElementById("result").textContent = `${inputTemperature} degrees Fahrenheit = ${convertedTemperature}degrees Celsius.`
+    
+    } else
+    
+    {
+        document.getElementById("result").textContent='please select a valid unit.';
+    }
+
+    
+
+
+    }
